bea1a67c96022b6705b30c94d5b987365014d5d1
zmzhydron <zmzhydron@outlook.com>
2017-07-18 22:43:44 +0800

    dsfg

--- localTest/stream.js
+++ localTest/stream.js
@@ -1,15 +1,19 @@
-var { Readable, Writable  } = require("stream")
+var { Readable, Writable, Duplex  } = require("stream")
 var ary = [1,2,3,4,4,5,6,6,'z','m','z','#'];
+var me = 'zhangmingzhi';
 class Read extends Readable{
 	constructor(ops){
 		super(ops)
+		this.datas = ary;
 	}
 	_read(){
-		for(var j of ary){
+		for(var j of this.datas){
 			this.push(j+"");
 		}
-		this.push("#");
 		this.push(null);
+	}
+	setdata(data){
+		this.datas = data;
 	}
 } 
 class Write extends Writable{
@@ -17,25 +21,69 @@ class Write extends Writable{
 		super();
 		this.datas = [];
 	}
-	_write(chunk,dev,next){
-		this.datas.push(chunk+"");
-		next();
+	_write(chunk,encoding,callback){
+		// var str = chunk.toString(dev)
+		this.datas.push(chunk.toString());
+		if(chunk.toString() === 'm'){
+			// next(throw new Error("fuckyoubitch"))
+			callback(new Error('故意报错'))
+			// callback()
+		}else{
+			callback();	
+		}
 	}
 }
-var rs = new Read();
-var ws = new Write();
-rs.on("data", (err,data) => {
-	console.log(err.toString())
-	ws.write(err.toString())
-	if(err.toString() === '#'){
-		// ws.end();
-		rs.close();
+class Dup extends Duplex{
+	constructor(ops){
+		super(ops)
+		this.list = ary;
+		this.results = [];
 	}
+	_read(){
+		for(var s of this.list){
+			this.push(s.toString())
+		}
+		this.push(null)
+	}
+	_write(chunk,dev,callback){
+		this.results.push(chunk.toString())
+		callback();
+	}
+}
+var dr = new Dup();
+var dw = new Dup();
+
+dr.on('data', data =>{
+	dw.write(data.toString())
 })
-rs.on("finish", (err,data) => {
+dr.on('end', (err, data) => {
+	dw.end();
+})
+dw.on("finish", (err, data) => {
+	console.log(dw.results, `dw.on("finish",`)
+})
+
+var rs = new Read();
+rs.setdata(Array.from(me))
+var ws = new Write();
+rs.on("data", (data) => {
+	if(data.toString() === '@'){
+		// ws.end("BitCH");
+		// rs.end();
+	}else{
+		ws.write(data.toString())	
+	}
+	
+})
+rs.on("end", (err,data) => {
 	if(err){
 		console.log('!!!!!!!!!!')
 	}else{
-			console.log(data, " finish ");		
+		console.log(" end ");	
+		ws.end();
 	}
+})
+// rs.pipe(ws)
+ws.on("finish", (err, data) =>{
+	console.log(ws.datas," ~~~~~~~~ ")
 })
