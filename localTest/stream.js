b3a7cc744e8defb293e665ec86c9cc5dc141bd4b
zmzhydron <zmzhydron@Outlook.com>
2017-07-19 17:48:52 +0800

    transform stream implementation success!

--- localTest/stream.js
+++ localTest/stream.js
@@ -1,4 +1,4 @@
-var { Readable, Writable, Duplex  } = require("stream")
+var { Readable, Writable, Duplex, Transform } = require("stream")
 var ary = [1,2,3,4,4,5,6,6,'z','m','z','#'];
 var me = 'zhangmingzhi';
 class Read extends Readable{
@@ -26,7 +26,7 @@ class Write extends Writable{
 		this.datas.push(chunk.toString());
 		if(chunk.toString() === 'm'){
 			// next(throw new Error("fuckyoubitch"))
-			callback(new Error('故意报错!!'))
+			// callback(new Error('故意报错!!'))
 			// callback()
 		}else{
 			callback();	
@@ -50,18 +50,29 @@ class Dup extends Duplex{
 		callback();
 	}
 }
+class Tras extends Transform{
+	constructor(options){
+		super(options)
+	}
+	_transform(chunk,dev,callback){
+		this.push(`@@@ ${chunk.toString()} @@@`)
+		// console.log(chunk, " _transform ")
+		callback();
+	}
+}
+var tra = new Tras();
 var dr = new Dup();
 var dw = new Dup();
 
-dr.on('data', data =>{
-	dw.write(data.toString())
-})
-dr.on('end', (err, data) => {
-	dw.end();
-})
-dw.on("finish", (err, data) => {
-	console.log(dw.results, `dw.on("finish",`)
-})
+// dr.on('data', data =>{
+// 	dw.write(data.toString())
+// })
+// dr.on('end', (err, data) => {
+// 	dw.end();
+// })
+// dw.on("finish", (err, data) => {
+// 	console.log(dw.results, `dw.on("finish",`)
+// })
 
 var rs = new Read();
 rs.setdata(Array.from(me))
@@ -71,9 +82,18 @@ rs.on("data", (data) => {
 		// ws.end("BitCH");
 		// rs.end();
 	}else{
-		ws.write(data.toString())	
+		// ws.write(data.toString())	
+		tra.write(data.toString())
 	}
-	
+})
+// rs.pipe(tra)
+tra.on("data", data => {
+	console.log(data.toString(), " tra ")
+	ws.write(data.toString())
+})
+tra.on('finish', (err, data) => {
+	// console.log(tra)
+	console.log(`tra.on('finish'`)
 })
 rs.on("end", (err,data) => {
 	if(err){
@@ -81,6 +101,7 @@ rs.on("end", (err,data) => {
 	}else{
 		console.log(" end ");	
 		ws.end();
+		tra.end();
 	}
 })
 // rs.pipe(ws)
