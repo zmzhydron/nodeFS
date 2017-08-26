090ae790d01e57f146924ecb8cb30609425ee2ea
zhangmingzhi <zmzhydron@outlook.com>
2017-08-26 23:07:40 +0800

    first linux commit

--- localTest/os.js
+++ localTest/os.js
@@ -1,7 +1,7 @@
 //http://nodejs.cn/api/os.html
 var os = require("os")
 var fs = require("fs")
-
+hehe
 // console.log(os.arch())
 // console.log(os.cpus()) //os.cpus() 方法返回一个对象数组, 包含安装的每个CPU/CPU核的信息.
 console.log(os.homedir(), "homedir") //os.homedir() 方法以字符串的形式返回当前用户的home目录.
