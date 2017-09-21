e1fda945928257257c0c5fa1d34a403c4d722874
zmzhydron <zmzhydron@Outlook.com>
2017-09-21 18:01:08 +0800

    asdf

--- localTest/pacong/autohome.js
+++ localTest/pacong/autohome.js
@@ -16,28 +16,9 @@ const preart = 5; //最大文章读取数
 const maxDrill = 5; //最大每个论坛往下查找5页
 
 
-const interest = ["宝马M", "AMG", "法拉利", "911", "阿斯顿", "奥迪RS", "兰博基尼", "宾利"];
+const interest = ["宝马M", "AMG", "法拉利论坛", "911", "阿斯顿", "奥迪RS", "兰博基尼", "宾利"];
 const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"];
-
-// console.log(__dirname, " )))))))))))) ")
-// console.log(path.relative(__dirname,path.resolve(__dirname, "../../client/src")), " )))))))))))) ")
-
-// if(cluster.isMaster){
-// 	os.cpus().forEach( (item, index) => {
-// 		cluster.fork()
-// 	})
-// 	cluster.on("listening", (worker, address) => {
-// 		console.log('listening: worker ' + worker.process.pid +', port: '+address.port);
-// 	})
-// 	cluster.on("exit", (worker, code, signal) => {
-// 		setTimeout( () => {
-// 			console.log(`worker : ${worker.process.id} 重启`)
-// 			worker.fork();
-// 		},1000);
-// 	})
-// }else{
-
-	const ROOTURL = 'http://club.autohome.com.cn';
+const ROOTURL = 'http://club.autohome.com.cn';
 
 	var rootPath = path.resolve(__dirname,`./../../../autohome`)
 	if(!fs.existsSync(rootPath)){
@@ -60,8 +41,12 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 		fs.writeFileSync(errorlog,"")
 	}
 	var domLog = path.resolve(rootPath, "./domLog.json");
-	if(!fs.existsSync(errorlog)){
-		fs.writeFileSync(errorlog,"")
+	if(!fs.existsSync(domLog)){
+		fs.writeFileSync(domLog,"")
+	}
+	var domLog1 = path.resolve(rootPath, "./domLog1.json");
+	if(!fs.existsSync(domLog1)){
+		fs.writeFileSync(domLog1,"")
 	}
 	var totoalSize = 0;
 	//过滤名称中的非法字符串，避免创建失败
@@ -69,11 +54,11 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 		if(!str){
 			return false;
 		}
-		return str.replace(/(\\|\/|\:|\*|\?|\"|\<|\>|\|\.|，|\.)+/g,"");
+		return str.replace(/(\\|\/|\:|\*|\?|\"|\<|\>|\|\.|，|\.|\|\|)+/g,"");
 	}
 	//获取原来已经下载的，如果有，就不在请求，提升速度
 	function getoldversion(){
-		return fs.readFileSync(cfg).toString("utf8").split(":").filter( item => item.trim());
+		return fs.readFileSync(cfg).toString("utf8").split("@").filter( item => item.trim());
 	}
 	var oldList = getoldversion();
 	var headers = {
@@ -81,7 +66,7 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 			  'Accept': `text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8`,
 			  'Accept-Encoding': `gzip, deflate`,
 			  'Accept-Language': `en-US,zh-CN;q=0.8,zh;q=0.6,en;q=0.4`,
-				"referer": "https://www.chiphell.com/",
+				// "referer": "https://www.chiphell.com/",
 			  // 'Cache-Control': 'no-cache',
 			  // "Host": url,
 			  // 'Pragma': `no-cache`,
@@ -100,30 +85,44 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 		}
 		return chunkList;
 	}
-	function dowmloadImage(url, dist){
-		var errorCount = 0;
-		function core(url,dist, resolve, reject, hashKey){
-			request.get(url)
-			.on('response', res => {
-				if(res.statusCode == 200){
-					totoalSize += parseInt(res.headers['content-length'])
-					// console.log(`下载图片${url}开始`)
-				}
+	function dowmloadImage(imgchunks){
+		function core(url, dist){
+			return new Promise( (resolve, reject) => {
+				request.get(url)
+				.on('response', res => {
+					if(res.statusCode == 200){
+						totoalSize += parseInt(res.headers['content-length'])
+						// console.log(`下载图片${url}开始`)
+					}
+				})
+				.on('end', val => {
+					// console.log(`${url}完成`)
+					resolve("ok");
+				})
+				.on('error', val => {
+					resolve("false")
+					console.log(`下载${url}失败`, val);
+					fs.appendFileSync(error1, `下载${url}失败;`);
+				})
+				.pipe(fs.createWriteStream(dist))
 			})
-			.on('end', val => {
-				// console.log(`${url}完成`)
-				resolve("ok");
-			})
-			.on('error', val => {
-				resolve("false")
-				console.log(`下载${url}失败`, val);
-				fs.appendFileSync(error1, `下载${url}失败;`);
-			})
-			.pipe(fs.createWriteStream(dist))
 		}
-		return new Promise( (resolve, reject) => {
-			core(url, dist, resolve, reject)
-		})
+		function chunks(chunk){
+			let chunkPromise = chunk.map( (item, index) => {
+				let { src, disk } = item;
+				return core(src, disk);
+			})
+			return Promise.all(chunkPromise)
+		}
+		async function go(imgchunks){
+			let list = [];
+			for(let s = 0; s<imgchunks.length; s++){
+				let r = await chunks(imgchunks[s]);
+				list = [...list, ...r]
+			}
+			return list;
+		}
+		return go(imgchunks)
 	}
 	function requestCore(url,callback = (...val) => val){
 		var options = {
@@ -160,10 +159,6 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 				var forms = Array.from($(".forum-list02 a"))
 				.filter((item, index) => {
 					let { attribs: attrs, parent, } = item;
-					if(index === 0){
-						// console.log(item.attribs.title);
-					}
-					// console.log(item.attribs.title);
 					return interest.filter( asdf => item.attribs.title.includes(asdf)).length;
 				})
 				.map( item => {
@@ -179,143 +174,75 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 	}
  	//解析每个论坛的帖子，并创建这个论坛的文件夹
 	function parseForms(obj){
-		//创建文件夹
-		let { url, title } = obj;
-		let dir = path.resolve(rootPath, `./${title}`);
-		if(!fs.existsSync(dir)){
-			fs.mkdirSync(dir);
-		}
-		return new Promise( (resolve, reject) => {
-			requestCore(url).then( val => {
-				let [ response, body, $,] = val;
-				let topics = Array.from($(".a_topic"))
-				.filter( (i,j) => {
-					let lang = i.parent.parent.attribs.lang;
-					if(lang){
-						lang = lang.split("|");
-						return lang[7] == "3" && lang[9] == "1";
-					}else{
-						return false;
+		function single(item){
+			//创建文件夹
+			let { url, title } = item;
+			let dir = path.resolve(rootPath, `./${title}`);
+			if(!fs.existsSync(dir)){
+				fs.mkdirSync(dir);
+			}
+			return new Promise( (resolve, reject) => {
+				requestCore(url).then( val => {
+					if(val == "0"){
+						resolve([]);
+						return 
 					}
-				})
-				.map( (i,j) => {
-					let topicName = filterDist(i.children[0].data.trim());
-					if(!topicName){
-						return false;
-					}
-					let topicSrc = ROOTURL+i.attribs.href;
-					let donwloadSrc = path.resolve(dir,`./${topicName}`);
-
-					if(j === 0){
+					let [ response, body, $,] = val;
+					let topics = Array.from($(".a_topic"))
+					.filter( (i,j) => {
+						let lang = i.parent.parent.attribs.lang;
+						if(lang){
+							lang = lang.split("|");
+							return lang[7] == "3" && lang[9] == "1";
+						}else{
+							return false;
+						}
+					})
+					.map( (i,j) => {
+						let topicName = filterDist(i.children[0].data.trim());
+						if(!topicName){
+							return false;
+						}
+						let topicSrc = ROOTURL+i.attribs.href;
+						let donwloadSrc = path.resolve(dir,`./${topicName}`);
 						if(!fs.existsSync(donwloadSrc)){
 							fs.mkdirSync(donwloadSrc)
 						}
-					}
-					return {
-						donwloadSrc,
-						topicName,
-						topicSrc
-					}
-				})
-				fs.writeFileSync(domLog, util.inspect(topics));
-				resolve(topics);
-			})	
-		})
-	}
-	function makeImages(articles){
-		async function processChunk(articles){
-			let chunkList = sliceToChunk(articles, preart);
-			let rlist = [];
-			for(let i = 0;i <chunkList.length; i++){
-					let r = await core(chunkList[i]);
-					rlist = [...rlist, ...r];
-
-			}
-			return rlist;
-		}
-		function core(articles){
-			let articlesPro = articles.map( (item, index) => {
-				return new Promise((resolve, reject) => {
-					let key = item.split(".com/")[1];
-					if(oldList.includes(key)){
-						console.log(`############ ${key} 已经存在, 快读！！！！`)
-						resolve([])
-					}else{
-						requestCore(item).then( val => {
-							if(val == "0"){
-								resolve([])
-								return;
-							}
-							let [ response, body, $,] = val;
-							let title = filterDist($("#thread_subject").html());
-							if(!title){
-								resolve([])
-								console.log("解析title失败!!!", item)
-								fs.appendFileSync(errorlog, item+"@"+$("#thread_subject")+";")
-								return;
-							}
-							//在window系统中如果路径含有特殊字符则会创建失败
-							let dist = path.resolve(rootPath, `./${title}`);
-							if(!fs.existsSync(dist)){
-								fs.mkdirSync(dist)
-								//获取单个文章中的全部主要图片；
-								console.log(`开始下载 ${title}`)
-						  	var chunk = Array.from($("img"))
-						  	.filter( item => {
-						  		let attrs = item.attribs;
-						  		if(attrs.zoomfile && attrs.file){
-						  			return attrs.file;
-						  		}
-						  	}).map( (item, index) => {
-						  		return {
-						  			url: `https://www.chiphell.com/${item.attribs.file}`,
-						  			dist: `${dist}/${index}.jpg`
-						  		}
-						  	})
-						  	resolve(chunk);
-						  	fs.appendFileSync(cfg,`${key}:`)
-						  	console.log(`***************** ${title} 解析完成, 写入cfg`)
-							}else{
-								fs.appendFileSync(cfg,`${key}:`)
-								console.log(`----------------- ${title}已经存在, 写入cfg`)
-								resolve([])
-							}
-						})
-					}
-				})
+						return {
+							donwloadSrc,
+							topicName,
+							topicSrc
+						}
+					})
+					resolve(topics);
+				})	
 			})
-			return Promise.all(articlesPro)
 		}
-		return processChunk(articles);
-	}
-	function donwloadChunk(chunkimgs){
-		let imgPromise = chunkimgs.map( (item, index) => {
-			let { url, dist } = item;
-			return dowmloadImage(url, dist)
-		})
-		return Promise.all(imgPromise)
-	}
-	//get all img infomations;
-	function request2(imglist){
-		let chunkList = sliceToChunk(imglist, prechunk)
-		async function core(chunkList){
-			for(let i = 0; i<chunkList.length; i++){
-				await donwloadChunk(chunkList[i])
+		function chunk(chunk){
+			return Promise.all(chunk.map( item => single(item)))
+		}
+		async function hehe(obj){
+			let topics = [];
+			for(let i = 0; i < obj.length; i++){
+				let r = await chunk(obj[i]);
+				let r_topics = r.reduce( (a,b) => {
+					return [...a,...b]
+				},[])
+				topics = [...topics, ...r_topics];
 			}
-			return "request2 完成";
+			return topics;
 		}
-		return core(chunkList)
+		return hehe(obj);
 	}
-	async function crawlCHH(){
-		var start = new Date().valueOf();
-		var forms = await getEveryForms(ROOTURL);
+	//每个论坛往下查找
+	function drill(forms,level){
 		//每个论坛往下查找5页
 		var allforms = [];
 		forms.forEach( (item, index) => {
 			let e = /-\d+\.html/;
 			let { url, title, } = item;
 			let count = 1;
-			while(count < (maxDrill + 1)){
+			while(count < (level + 1)){
 				url = url.replace(e, `-${count}.html`);
 				count++;
 				allforms.push({
@@ -324,21 +251,97 @@ const keyinterest = ["媳妇", "女友", "闺蜜", "性感", "黑丝", "肉丝"]
 				})
 			}
 		})
-		console.log(allforms)
-		var topics = await parseForms(allforms[4]);
-		// articles = [...articles.slice(0,40)];
-		// console.log(articles)
-		// var imglist = await makeImages(articles)
-		// imglist = imglist.reduce( (a, b) => [...a, ...b], [])
-		// console.log('~~~~~~~~~~~~~~~~~~');
+		return allforms;
+	}
+	//找到全部的img
+	function processImage(topics){
+		function core(item){
+			let { donwloadSrc, topicSrc, topicName } = item;
+			return new Promise( (resolve, reject) => {
+				if(oldList.includes(topicName)){
+					resolve([])
+					console.log("已经有了主题:"+topicName, "跳过")
+					return;
+				}
+				requestCore(topicSrc).then( val => {
+					if(val == "0"){
+						resolve([]);
+						return 
+					}
+
+					let [ response, body, $,] = val;
+					let imgs = Array.from($("img"))
+					.filter( (item, index) => {
+						let { attribs: { name = false, src9, src = "", } } = item;
+						return src.includes("club2.autoimg.cn/album") ? true : (name === "lazypic" && src9);
+					})
+					.map( (item, index) => {
+						let { attribs: { src9, src = "", } } = item;
+						return {
+							disk: `${donwloadSrc}/${index}.jpg`,
+							src: src.includes("club2.autoimg.cn/album") ? src : src9
+						}
+					})
+					fs.appendFileSync(cfg, `${topicName}@`);
+					resolve(imgs)
+				})
+			})
+		}
+		function chunk(chunk){
+			let chunkPromise = chunk.map( (item, index) => {
+				return core(item);
+			})
+			return Promise.all(chunkPromise)
+		}
+		async function hehe(obj){
+			let list = [];
+			for(let i = 0; i < obj.length; i++){
+				let r = await chunk(obj[i]);
+				let r_list = r.reduce( (a,b) => {
+					return [...a,...b]
+				},[])
+				list = [...list, ...r_list];
+			}
+			return list;
+		}
+		return hehe(topics);
+	}
+	async function crawl(){
+		var start = new Date().valueOf();
+
+		var forms = await getEveryForms(ROOTURL);
+		var allforms = drill(forms,2);
+		var topics = await parseForms(sliceToChunk(allforms, 2));
+		topics = sliceToChunk(topics, 2);
+		topics = [...topics.slice(0,40)]
+		var imgs = await processImage(topics)
+		console.log(imgs.length, " *************************  ")
+		console.log('~~~~~~~~~~~~~~~~~~');
+		var r = await dowmloadImage(sliceToChunk(imgs, 50))
 		// var r = await request2(imglist);
 		// console.log("爬虫用时: ", new Date().valueOf() - start);
 		// return r;
-		return topics
+		fs.appendFileSync(domLog, util.inspect(topics));
+		return topics;
 	}
-	crawlCHH().then( val => {
+	crawl().then( val => {
 		let totoal = totoalSize / 1000 * 1000;
-		console.log(val.length)
-		// console.log(`*(************${val}*****************`, totoal, totoalSize)
+		// console.log(val)
+		console.log(`*(************ok!!!!!*****************`, totoal, totoalSize)
 	})
 // }
+
+
+				// request.get(`http://club2.autoimg.cn/album/g7/M00/F6/3E/userphotos/2017/07/05/12/500_wKjB0FlcakiAIKO4AAir2ZODQ8s742.jpg`)
+				// .on('response', res => {
+				// 	if(res.statusCode == 200){
+				// 		console.log(`下载图片开始`)
+				// 	}
+				// })
+				// .on('end', val => {
+				// 	console.log(`完成`)
+				// })
+				// .on('error', val => {
+				// 	console.log(`下载失败`, val);
+				// })
+				// .pipe(fs.createWriteStream('C:\\Users\\zmz\\Desktop\\Github\\autohome\\阿斯顿·马丁论坛\\『最爱还是自己的信仰』阿斯顿马丁VantageV8换车贴~/5.jpg'))
